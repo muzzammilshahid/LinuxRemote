905ffa983e7fa178f8f3815b8ab212944792eea5
muzzammil <muzzammil.shahid.311@gmail.com>
2021-07-24 16:17:29 +0500

    bash script added

---
+++ LinuxRemote.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+sudo apt install libasound2-dev portaudio19-dev libportaudio2 libportaudiocpp0 ffmpeg:i386 ffmpeg
+
+sudo apt-get install -y python-dbus-dev
+sudo apt-get install libglib2.0-*
+
+pip3 install dbus-python
+pip3 install Pillow
