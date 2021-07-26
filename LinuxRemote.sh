e051486c61f8d3997421905e69ea2d114772091d
muzzammil <muzzammil.shahid.311@gmail.com>
2021-07-26 12:06:46 +0500

    bash edited

--- LinuxRemote.sh
+++ LinuxRemote.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
+sudo apt update
+sudo apt upgrade
+sudo apt install python3-pip
 sudo apt install libasound2-dev portaudio19-dev libportaudio2 libportaudiocpp0 ffmpeg
 
 sudo apt-get install -y python-dbus-dev
 sudo apt-get install libglib2.0-*
 
-pip3 install dbus-python
-pip3 install Pillow
+sudo pip3 install -r requirements.txt
